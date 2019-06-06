8d1c6a90174abc27c0669ff377ed9e4b1bb14cfa
zfno111 <zfno111@163.com>
2019-06-06 10:15:49 +0000

    spider_jingc

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import requests
-
+#进程池
+from multiprocessing import Pool
 import time
 
 
@@ -31,57 +32,56 @@ class hupubxj_spider():
             print("用户的性别是:  %s" % (userSex))
 
 
-
         def getTieZiInfo(self,pagenum):
-
             # 中间写上代码块
-            global startTime
-            global stopTime
-            startTime = time.clock()
             headers=self.headers
             self.pagenum=pagenum
 
-            for i in (1,self.pagenum):
-                print(i)
-                #需要转换为str
-                URL="https://bbs.hupu.com/bxj-"+str(i)
-                respoense=requests.get(url=URL,headers=headers)
+            #需要转换为str
+            URL="https://bbs.hupu.com/bxj-"+str(self.pagenum)
+            respoense=requests.get(url=URL,headers=headers)
 #将获取到的数据转化为soup对象
-                soup = BeautifulSoup(respoense.text, 'lxml')
+            soup = BeautifulSoup(respoense.text, 'lxml')
 # 找到所有的a标签，特征是 class_="truetit"
-       #     print(soup.find_all('a',class_="truetit"))
+   #     print(soup.find_all('a',class_="truetit"))
 #结果是一个列表  (实际上就是Tag列表）
-        #    print(type(soup.find_all('a',class_="truetit")))
-                for p in soup.find_all('a',class_="truetit"):
-                    print("-----------------开始---华丽的分割线-----------")
+    #    print(type(soup.find_all('a',class_="truetit")))
+            for p in soup.find_all('a',class_="truetit"):
+                print("-----------------开始---华丽的分割线-----------")
 # 获取a标签的内容
-                    print("帖子的内容如下:   ")
+                print("帖子的内容如下:   ")
 #获取帖子的内容
-                    postInformation=p.get_text()
-                    print(postInformation)
-                    parentInfo=p.parent
-                    if parentInfo.find("span",class_="light_r"):
-                        # print(parentInfo.find("span",class_="light_r").a["title"])
+                postInformation=p.get_text()
+                print(postInformation)
+                parentInfo=p.parent
+                if parentInfo.find("span",class_="light_r"):
+                    # print(parentInfo.find("span",class_="light_r").a["title"])
 #获取热门回帖,没有就为0
-                        light_r=parentInfo.find("span", class_="light_r").a["title"]
-                        print("帖子目前的热度为: %s"%(light_r))
-                    else:
-                        light_r=0
+                    light_r=parentInfo.find("span", class_="light_r").a["title"]
+                    print("帖子目前的热度为: %s"%(light_r))
+                else:
+                    light_r=0
 #获取作者的信息--名字
-                    grandPaInfo=parentInfo.parent
-                    userName=grandPaInfo.find("div",class_="author box").a.get_text()
-                    print("用户的名字是: %s"%(userName))
+                grandPaInfo=parentInfo.parent
+                userName=grandPaInfo.find("div",class_="author box").a.get_text()
+                print("用户的名字是: %s"%(userName))
 #获取用户的主页
-                    userUrl=grandPaInfo.find("div",class_="author box").a['href']
-                    print("%s的主页地址是:   %s  "%(userName,userUrl))
-                    self.userInfo(userUrl)
-                print("-----------------第%s页爬取结束--------"%(i))
-                stopTime = time.clock()
-                print('Running time: %s Seconds' % (stopTime - startTime))
+                userUrl=grandPaInfo.find("div",class_="author box").a['href']
+                print("%s的主页地址是:   %s  "%(userName,userUrl))
+                self.userInfo(userUrl)
+            print("-----------------第%s页爬取结束--------"%(i))
 
 
+if __name__ == '__main__':
+    AAA=hupubxj_spider()
+    #输入的是字符串必须要转换为int 后续需要range使用
+    pageNum=int(input("请输入您想要爬去的步行街的页数： "))
+    p = Pool(4)
+    startTime = time.time()
+    for i in range(pageNum):
+        p.apply_async(AAA.getTieZiInfo,args=(i,))
+    p.close()
+    p.join()
+    stopTime = time.time()
+    print('Running time: %0.2f Seconds' % (stopTime - startTime))
 
-AAA=hupubxj_spider()
-#输入的是字符串必须要转换为int 后续需要range使用
-pageNum=int(input("请输入您想要爬去的步行街的页数： "))
-AAA.getTieZiInfo(pageNum)
